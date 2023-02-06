f91f20cf21f355eb551ce6622d70d4ec6d5c5b14
Laura Nunes <lauranunes2508@gmail.com>
2023-02-06 16:01:24 -0300

    Project created

---
+++ src/main/java/com/excercise/todolist/demo/ProjectToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.excercise.todolist.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excercise/todolist/demo/ProjectToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excercise.todolist.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
