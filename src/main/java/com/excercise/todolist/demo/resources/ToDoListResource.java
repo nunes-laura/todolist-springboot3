9d0c53aad65590253274e92cb01271038234df7e
Laura Nunes <lauranunes2508@gmail.com>
2023-02-07 11:53:50 -0300

    Service layer, component registration

--- src/main/java/com/excercise/todolist/demo/resources/ToDoListResource.java
+++ src/main/java/com/excercise/todolist/demo/resources/ToDoListResource.java
@@ -1,22 +1,35 @@
 package com.excercise.todolist.demo.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.excercise.todolist.demo.entities.ToDoList;
-import com.excercise.todolist.demo.entities.enums.TaskStatus;
+import com.excercise.todolist.demo.services.ToDoListService;
 
 @RestController
 @RequestMapping(value="/newlist")
 public class ToDoListResource {
 	
+	@Autowired
+	private ToDoListService service;
+	
 	
 	@GetMapping
-	public ResponseEntity<ToDoList> findAll () {
-		ToDoList t = new ToDoList(1, "Clean the bathroom", TaskStatus.READY_TO_DO, "Mop the floor and clean the mirrors");
-		return ResponseEntity.ok().body(t);
+	public ResponseEntity<List<ToDoList>> findAll () {
+		List<ToDoList> tl =  service.findAll();
+		return ResponseEntity.ok().body(tl);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity <ToDoList> findById (@PathVariable Integer id) {
+		ToDoList obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 
---
+++ src/main/java/com/excercise/todolist/demo/services/ToDoListService.java
@@ -0,0 +1,29 @@
+package com.excercise.todolist.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.excercise.todolist.demo.entities.ToDoList;
+import com.excercise.todolist.demo.resources.repositories.ToDoListRepository;
+
+
+@Service
+public class ToDoListService {
+	
+	@Autowired
+	private ToDoListRepository repository;
+	
+	public List<ToDoList> findAll() {
+		return repository.findAll();
+	}
+	
+	public ToDoList findById(Integer id) {
+		Optional<ToDoList> obj =  repository.findById(id);
+		return obj.get();	}
+	
+	
+
+}
